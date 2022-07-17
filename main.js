15f9e178f2e299aa42ee5219b67d1f37999f29fd
bensonirah <bensonirah@gmail.com>
2022-07-17 12:57:26 +0300

    correction exo-1

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const printStringMethod = require("./lib/string_method");
+// verify email
 
-printStringMethod();
+const verifyEmail = require("./lib/exo-1/email-validator");
+
+// ^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$
+const email = "person@company";
+const response = verifyEmail(email);
+
+console.log(response);
