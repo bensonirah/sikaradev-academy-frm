4fd5d3851167531a4ae77beb8620d026e3e8c8f5
bensonirah <bensonirah@gmail.com>
2022-08-21 10:11:16 +0300

    setting up boolean-regex

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-
-// verify email
-
-const verifyEmail = require("./lib/exo-1/email-validator");
-
-// ^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$
-const email = "person@company";
-const response = verifyEmail(email);
-
-console.log(response);
+console.log(new Boolean(false) === new Boolean(false));
