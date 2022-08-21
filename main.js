c845ba93d6aa21173d3203b64018d38d2b68f48e
bensonirah <bensonirah@gmail.com>
2022-08-21 13:10:00 +0300

    setting up error-story

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-
-// verify email
-
-const verifyEmail = require("./lib/exo-1/email-validator");
-
-// ^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$
-const email = "person@company";
-const response = verifyEmail(email);
-
-console.log(response);
+carName = "Volvo";
+var carName;
